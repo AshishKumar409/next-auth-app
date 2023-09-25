eb2e1d55b3928649c907c479ef69e3ecf2dbfa5e
unknown <ashishkumarnath9@gmail.com>
2023-09-25 12:48:06 +0530

    initial commit

---
+++ src/app/login/page.tsx
@@ -0,0 +1,11 @@
+
+
+export default function page() {
+    return (
+        <div className='flex justify-center'>
+            <h1>
+                Login page
+            </h1>
+        </div>
+    )
+}
---
+++ src/app/signup/page.tsx
@@ -0,0 +1,11 @@
+
+
+export default function page() {
+    return (
+        <div className='flex justify-center'>
+            <h1>
+                Signup page
+            </h1>
+        </div>
+    )
+}
